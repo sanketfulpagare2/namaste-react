7f2cc4330aa6d5335831071da9e8412e920b83a0
sanketfulpagare2 <sanketfulpagare2@gmail.com>
2023-12-16 23:44:15 +0530

    episode-03

--- App.js
+++ App.js
@@ -1,18 +1,50 @@
 import React from "react";
 import  ReactDOM  from "react-dom/client";
 
-const parent =React.createElement("div",{id:"parent"},[
-    React.createElement("div",{id:"child1"},[
-        React.createElement("h1",{},"hello im h1 of child 1"),
-        React.createElement("h2",{},"hello im h2 of child 1"),
+const jsxHeading= (<h1 className="heading">Hello React From jsx</h1>);
 
-    ]),
-    React.createElement("div",{id:"child2"},[
-        React.createElement("h1",{},"hello im h1 of child 2"),
-        React.createElement("h2",{},"hello im h2 of child 2"),
 
-    ])
-    
-]);
+
+
+
+
+const Title =() => ( 
+<h1>hello from comp 1</h1>
+);
+
+
+
+const Title2 =() => {
+    return <>
+    <div className="heading2">
+
+        <h1 >this 2nd type of Component</h1>
+    </div>
+    </>
+};
+
+
+
+const Heading = () => (
+    <div className="container">
+        {jsxHeading}
+
+        <Title />
+
+        // ! AT the end of day it is JS function
+        {Title()}
+        
+        <Title2 />
+
+         <h2>I am a Component RC </h2>
+    </div>
+);
+
+
+
+
 const root =ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+
+
+root.render(<Heading/>);
