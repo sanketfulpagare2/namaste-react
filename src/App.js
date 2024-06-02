700ae8071003e958fed0ba6e7bdab6168cd4f3a6
sanketfulpagare2 <sanketfulpagare2@gmail.com>
2024-06-02 23:58:43 +0530

    canclled  all

--- src/App.js
+++ src/App.js
@@ -14,16 +14,6 @@ import UserContext from "./utils/UserContext";
 import appStore from "./utils/appStore";
 import Cart from "./components/Cart";
 
-const express=require("express");
-const cors = require("cors");
-const app = express();
-
-app.use(
-    cors({
-        origin: "*",
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        })
-);
 
 
 
