4bf8ed857d45ffcf057b93b6d76b70a3eeb1f92f
sanketfulpagare2 <sanketfulpagare2@gmail.com>
2024-06-04 23:34:20 +0530

    back to old

--- src/utils/useResList.js
+++ src/utils/useResList.js
@@ -12,9 +12,9 @@ const useResList = () => {
     }, []);
   
     const fetchData = async () => {
-      const data = await fetch("https://namaste-react-lzi1em1qb-sanketfulpagare2s-projects.vercel.app/api/restaurants?lat=21.11610&lng=79.07060"
-        // "https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.11610&lng=79.07060&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-        // "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5642452&lng=73.7768511&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      const data = await fetch(
+        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.11610&lng=79.07060&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+        
       );
       const json = await data.json();
       console.log(json.data?.cards[0]?.card?.card?.imageGridCards?.info)
